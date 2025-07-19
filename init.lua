468b59681dab08fe275c823883c84c07ac918c59
yousof shahrabi <yousof.shahrabi84@gmail.com>
2025-07-19 13:13:22 +0330

    add supermaven, add keybind for moving in file

--- init.lua
+++ init.lua
@@ -85,6 +85,13 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Moving Vertically
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- Greatest remap ever
+vim.keymap.set('x', '<leader>p', '"_dP')
+
 -- [[ Basic Autocommands ]]
 
 -- Highlight when yanking (copying) text
